c92d29522f8ee69c13f0ed25e2a3bc9c90d8e13e
jiri-jarema <jiri.jarema@gmail.com>
2025-08-17 16:21:24 +0200

    Add files via upload

--- js/state.js
+++ js/state.js
@@ -1,131 +1,41 @@
-// This file manages the global state of the application.
+// js/state.js
 
-let currentUser = null;
-let userRole = null;
-let userOdbor = null;
-let assetData = {};
-let sharedOptions = {};
-let allAssets = {};
-let parentMap = {};
-
-// Constants
-export const detailOrder = [
-    "Garant", "Účel zpracování", "Zákonnost zpracování", "Legislativa", "Regulované služby", "Způsob zpracování",
-    "Zdroje osobních údajů", "Kategorie osobních údajů", "Zvláštní kategorie osobních údajů",
-    "Lhůty pro výmaz", "Zpracovatel", "Jmenný seznam oprávněných osob",
-    "Kategorie příjemců osobních údajů", "Propojení na jiné správce nebo zpracovatele",
-    "Předávání osobních údajů do třetí země", "Zabezpečení zpracování - elektronické",
-    "Zabezpečení zpracování - analogové"
-];
-
-// Nově přidané konstanty pro sjednocení pořadí
-export const serviceDetailOrder = [
-    "Legislativa", "Agendový informační systém", "Agendy"
-];
-
-export const infoSystemDetailOrder = [
-    "Popis", "Vlastník", "SpravceNeboZastupce", "Uživatelé", "Stav aktiva", "Koncepce",
-    "Přístup", "Nedostupnost", "Klasifikace informací", "Aplikační server", "Databáze",
-    "Sítě", "Regulovaná služba", "Agendy", "Zabezpečení"
-];
-
-export const defaultSupportAssetOrder = [
-    "Vlastnik", "Spravce_zastupce", "Stav_aktiva", "Datum_porizeni", "Termin_obnovy", "SLA", "Model",
-    "Funkce", "Operacni_system", "Vybaveni", "Provozovane_informacni_systemy", "Provozovane_databaze",
-    "Informacni_systemy_vyuzivajici_DB", "Server", "Cil_zalohovani", "Frekvence_zalohovani", "Verze",
-    "Informacni_systemy", "Poznamka"
-];
-
-
-export const reciprocalMap = {
-    'agendy': {
-        'Regulované_služby': {
-            targetCategoryPath: 'primarni/children/sluzby',
-            reciprocalField: 'Agendy'
-        }
-    },
-    'podpurna/children/databaze': {
-        'Informacni_systemy_vyuzivajici_DB': {
-            targetCategoryPath: 'primarni/children/informacni-systemy',
-            reciprocalField: 'Databaze'
-        },
-        'Cil_zalohovani': {
-            targetCategoryPath: 'podpurna/children/servery',
-            reciprocalField: 'Zalohovane_databaze'
-        },
-        'Server': {
-            targetCategoryPath: 'podpurna/children/servery',
-            reciprocalField: 'Provozovane_databaze'
-        }
-    },
-    'podpurna/children/servery': {
-        'Provozovane_databaze': {
-            targetCategoryPath: 'podpurna/children/databaze',
-            reciprocalField: 'Server'
-        },
-        'Provozovane_informacni_systemy': {
-            targetCategoryPath: 'primarni/children/informacni-systemy',
-            reciprocalField: 'Aplikační_server'
-        },
-        'Zalohovane_databaze': {
-             targetCategoryPath: 'podpurna/children/databaze',
-             reciprocalField: 'Cil_zalohovani'
-        }
-    },
-    'podpurna/children/site': {
-        'Informacni_systemy': {
-            targetCategoryPath: 'primarni/children/informacni-systemy',
-            reciprocalField: 'Sítě'
-        }
-    },
-    'primarni/children/informacni-systemy': {
-        'Agendy': {
-            targetCategoryPath: 'agendy',
-            reciprocalField: 'Způsob zpracování'
-        },
-        'Databaze': {
-            targetCategoryPath: 'podpurna/children/databaze',
-            reciprocalField: 'Informacni_systemy_vyuzivajici_DB'
-        },
-        'Aplikační_server': {
-            targetCategoryPath: 'podpurna/children/servery',
-            reciprocalField: 'Provozovane_informacni_systemy'
-        },
-        'Regulovaná_služba': {
-            targetCategoryPath: 'primarni/children/sluzby',
-            reciprocalField: 'Agendový informační systém'
-        },
-        'Sítě': {
-            targetCategoryPath: 'podpurna/children/site',
-            reciprocalField: 'Informacni_systemy'
-        }
-    },
-    'primarni/children/sluzby': {
-        'Agendy': {
-            targetCategoryPath: 'agendy',
-            reciprocalField: 'Regulované služby'
-        },
-        'Agendový_informační_systém': {
-            targetCategoryPath: 'primarni/children/informacni-systemy',
-            reciprocalField: 'Regulovaná služba'
-        }
+// The global state object for the application
+let state = {
+    data: null,
+    selectedAssetPath: null,
+    isEditing: false,
+    user: null,
+    userRole: null,
+    options: {
+        personalDataCategories: [
+            "Jméno a příjmení",
+            "Datum narození",
+            "Rodné číslo",
+            "Adresa",
+            "Kontakt (telefon, e-mail)",
+            "Bankovní spojení",
+            "IP adresa",
+            "Cookies",
+            "Jiné"
+        ],
+        recipientCategories: [
+            "Orgány veřejné moci",
+            "Zdravotní pojišťovny",
+            "Banky",
+            "Exekutoři",
+            "Jiné"
+        ]
     }
 };
 
-// Getters
-export const getCurrentUser = () => currentUser;
-export const getUserRole = () => userRole;
-export const getUserOdbor = () => userOdbor;
-export const getAssetData = () => assetData;
-export const getSharedOptions = () => sharedOptions;
-export const getAllAssets = () => allAssets;
-export const getParentMap = () => parentMap;
+/**
+ * Updates the global state by merging the new state with the existing state.
+ * @param {object} newState - An object containing the new state properties to merge.
+ */
+function setState(newState) {
+    state = { ...state, ...newState };
+}
 
-// Setters
-export const setCurrentUser = (user) => { currentUser = user; };
-export const setUserRole = (role) => { userRole = role; };
-export const setUserOdbor = (odbor) => { userOdbor = odbor; };
-export const setAssetData = (data) => { assetData = data; };
-export const setSharedOptions = (options) => { sharedOptions = options; };
-export const setAllAssets = (assets) => { allAssets = assets; };
-export const setParentMap = (map) => { parentMap = map; };
+// Export the state object and the setState function to make them available to other modules.
+export { state, setState };
