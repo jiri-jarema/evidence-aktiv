86118783927339a42894789a6fe4d068dd318101
jiri-jarema <jiri.jarema@gmail.com>
2025-10-13 14:38:44 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import { initAuth } from './auth.js';
-
-// Initialize the application by setting up authentication listener
-document.addEventListener('DOMContentLoaded', () => {
-    initAuth();
-});
